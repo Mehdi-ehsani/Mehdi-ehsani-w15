5b11cc098fd483a84b603c699c95f6cf6e4ad9e9
Mehdi-ehsani <metti.work@gmail.com>
2024-09-11 14:30:20 +0330

    fix(main.jsx) change name to Input.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-
+import Input from "./Input.jsx";
+import data from "./cities.json"
 function App() {
-   return <div></div>
+   return <div><Input/><h1>{console.log(data)}</h1></div>
 }
 
 export default App;
