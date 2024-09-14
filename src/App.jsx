9e6a2d528d7df2d0093710f2c3c922bab8d5abe9
Mehdi-ehsani <metti.work@gmail.com>
2024-09-14 13:14:23 +0330

    fix: show hint bug in city name

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Input from "./Input.jsx";
 import data from "./cities.json";
 function App() {
    const [value , setValue] = useState("");
    const [hint , setHint] = useState("");
-
+   const [showHint, setShowHint] = useState(false)
+   
+   useEffect(() => {
+     !showHint && setHint("")
+   },[value])
+   
 	const changeHandler = (e) => {
      const searchedValue = e.target.value; 
      setValue(searchedValue);
+     setShowHint(false)
      data.filter(city => {
-      city.startsWith(searchedValue) && setHint(city)
+      if(city.startsWith(searchedValue)) {
+         setShowHint(true)
+         setHint(city)
+      }
      })
      !searchedValue.length && setHint("")
    };
